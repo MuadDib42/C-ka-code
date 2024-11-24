b6f0d16b0389dcbd533187714c5a4582661c3e83
MuadDib42 <cyberpistol1@gmail.com>
2024-11-24 15:22:48 +0530

    Update matrix.c
    matrix multiplication

--- matrix.c
+++ matrix.c
@@ -1,39 +1,59 @@
 #include <stdio.h>
-
 void main(){
-    int r,c;
-    printf("Enter the no of rows:");
-    scanf("%d",&r);
-    printf("Enter the no of columns:");
-    scanf("%d",&c);
-    int a[r][c];
-    int x=0;
-    int count=0;
-    goto varall;
-    varall:
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(x==0){
-                printf("Enter the element at position [%d][%d]:",i,j);
-                scanf("%d",&a[i][j]);
-                }
-                else{
-                    if(count<r)
-                    {
-                        printf("%d", a[i][j]);
-                        count++;
-                    }
-                }
+    int r1,c1,r2,c2;
+    printf("Enter r1,c1:");
+    scanf("%d,%d",&r1,&c1);
+    int a1[r1][c1];
+    printf("Enter r2,c2:");
+    scanf("%d,%d",&r2,&c2);
+    int a2[r2][c2];
+    for(int i=0;i<r1;i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            printf("For matrix 1 enter %d%dth element:",i,j);
+            scanf("%d",&a1[i][j]);            
+        }
+        
+    }
+    for(int i=0;i<r2;i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            printf("For matrix 2 enter %d%dth element:",i,j);
+            scanf("%d",&a2[i][j]);            
+        }
+        
+    }
+    if(c1!=r2){
+        printf("The matrices are not multplicable");
+    }
+    else{
+        int c[r1][c2];
+        for(int i=0;i<r1;i++){
+            for(int j=0;j<c2;j++)
+            {
+                for(int k=0;k<c1;k++){
+                    c[i][j]=0;
+                    c[i][j]+=a1[i][k]*a2[k][j];
+                }                
+                
             }
         }
-    x=1;
-    printf("\n\nThe matrix is:\n");
-    goto varall;
-    int com=0;
-    int calc=0;
-    while(com==0){
-        printf("Enter 1 to multiply to itself, 2 to add to itself, 3 to substract itself:");
-        scanf("%d",&calc)
+        printf("Here is the new matrix:\n");
+        for(int i=0;i<r1;i++)
+        {
+            printf("[");
+            for(int j=0;j<c2;j++)
+            {
+                printf("%d ",c[i][j]);
+            }
+            printf("]");
+            printf("\n");
+            
+            
+        }
+
     }
 
 }
