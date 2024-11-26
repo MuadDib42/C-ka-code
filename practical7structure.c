753953e283d1c73ab97ca863534150864910d842
MuadDib42 <cyberpistol1@gmail.com>
2024-11-26 13:44:58 +0530

    Create practical7structure.c

---
+++ practical7structure.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+struct emp
+{   
+    int sal;
+    char emailid[40];
+    char address[100];
+    char empname[30];
+};
+int main(){
+    struct emp e[10];
+    for(int i=0;i<10;i++){
+        printf("enter salary no:");
+        scanf("%d",&e[i].sal);
+        printf("Enter empname:");
+        scanf("%s",e[i].empname);
+        printf("enter Address:");
+        scanf("%s",e[i].address);
+        printf("Enter email:");
+        scanf("%s",e[i].emailid);
+
+    }
+    int count=0;
+    for(int i=0;i<10;i++){
+        if(e[i].sal>10000){
+            count++;
+        }
+    }
+    printf("No of employees having more than 10000 as salary are %d",count);
+    return 0;
+
+}
